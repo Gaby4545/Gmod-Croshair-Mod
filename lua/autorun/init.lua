cb9c94f424de34592b894f070048c993077feb15
Gaby4545 <g845@live.ca>
2015-06-18 12:04:56 -0400

    Added a Comment

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,9 +1,6 @@
-// Made by Gaby4545 on steam
 AddCSLuaFile()
-/*
-I'm not good at making mods. This is my first one so it might not be optimized and clean.
-*/
 
+// Print the version in console with a nice green text
 local version = "0.01"
 MsgC(color_green, "Gaby4545's Crosshair mod version " .. version .. " has loaded!\n")
 
